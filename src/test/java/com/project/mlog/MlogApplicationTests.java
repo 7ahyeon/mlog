a9ec36bad3017ce94926858e3ab9a167dad3f208
조아현 <7ahyeon@gmail.com>
2023-08-22 16:45:27 +0900

    initial commit

---
+++ src/test/java/com/project/mlog/MlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
